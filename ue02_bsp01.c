55be5ce9d5567349a3fc36832243018f5beb5c0d
nina <nina@ninas-mbpro.home>
2014-03-12 18:39:39 +0100

    starting

---
+++ ue02_bsp01.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   ue02_bsp01.c
+ * Author: nina
+ *
+ * Created on 12. März 2014, 18:34
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
